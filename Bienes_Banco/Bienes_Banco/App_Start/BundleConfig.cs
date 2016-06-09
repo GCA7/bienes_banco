d1d9d3287080c5a28aec2a9aa41aaaccd95b55bd
Greivin Calvo Aguilar <Greivin Calvo Aguilar>
2016-06-09 10:57:25 -0600

    se agrega materialize al proyecto

--- Bienes_Banco/Bienes_Banco/App_Start/BundleConfig.cs
+++ Bienes_Banco/Bienes_Banco/App_Start/BundleConfig.cs
@@ -22,6 +22,7 @@ namespace Bienes_Banco
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/materialize.min.css",
                       "~/Content/site.css"));
         }
     }
